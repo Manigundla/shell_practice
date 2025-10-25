b6fb777f8f992df5b194304a5215508fba4878c4
mani G <gundlamanikumar@gmail.com>
2025-10-25 15:07:21 -0300

    added command line argus

--- variables01.sh
+++ variables01.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-x=Mani
-y=Pavan
-z=jaya
+x=$1
+y=$2
+z=$3
 
     echo "Hello, My name is $x. May I know your name?"
     echo "Hello, Mr $x. My name is $y, and here is my friend Mr.$z"
