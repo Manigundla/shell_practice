94f7ff90eb6a3b6d74b37232c2157fbd79965678
mani G <gundlamanikumar@gmail.com>
2025-10-27 13:53:12 -0300

    added a array script

--- Arrays.sh
+++ Arrays.sh
@@ -2,7 +2,7 @@
 
 Fruit=("apple" "mango" "banaya")
 
-    echo "{$Fruit[0]}"
-    echo "{$Fruit[1]}"
-    echo "{$Fruit[2]}"
-    echo "{$Fruit[@]}"
+    echo "${Fruit[0]}"
+    echo "${Fruit[1]}"
+    echo "${Fruit[2]}"
+    echo "${Fruit[@]}"
