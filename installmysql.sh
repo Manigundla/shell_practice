f2e8db5ed36d07bb15010d88a9b51911eec11979
mani G <gundlamanikumar@gmail.com>
2025-10-28 09:41:50 -0300

    made some changes to mysql script

--- installmysql.sh
+++ installmysql.sh
@@ -5,7 +5,16 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then 
     echo  "you are not a root user"
-else 
+    exit 1
     echo "you are root user"
 fi
+
 yum install mysql -y
+
+if [ $? -eq 0 ]
+then    
+    echo "installing mysql is successful"
+else 
+    echo "installing is failure"
+    exit 1
+fi    
