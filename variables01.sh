83c62265e39c0349c5f818f0fddedaf7b14c5cf7
mani G <gundlamanikumar@gmail.com>
2025-10-28 10:55:33 -0300

    made some changes to mysql script

--- variables01.sh
+++ variables01.sh
@@ -6,3 +6,9 @@ z=$3
 
     echo "Hello, My name is $x. May I know your name?"
     echo "Hello, Mr $x. My name is $y, and here is my friend Mr.$z"
+
+echo "all arguments passed:- $@"
+echo "number of arguments passed:- $#"
+echo "exit status:- $?"
+echo "N number of argumnets:- $N"
+echo "name of the argumnet:- $0"
