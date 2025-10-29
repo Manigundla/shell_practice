d1c7453446f500d33a6b68c0897c46116e07e610
Manigundla <gundlamanikumar@gmail.com>
2025-10-29 09:57:30 -0300

    Update logs.sh

--- logs.sh
+++ logs.sh
@@ -2,12 +2,13 @@
 
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-
-LOGFILE="/tmp/$0-$TIMESTAMP.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
 
 validate(){
     if [ $1 -ne 0 ]
