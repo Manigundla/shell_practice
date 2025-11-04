5baacfb8681f012a721b9e8844b640bdaa518b74
mani G <gundlamanikumar@gmail.com>
2025-11-04 09:07:39 -0400

    addding a script

--- Installingpackages.sh
+++ Installingpackages.sh
@@ -6,10 +6,10 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-if [ $ID eq 0 ]
+if [ $ID -eq 0 ]
 then 
-    echo "$G you ara a root user $N"
+    echo -e "$G you ara a root user $N"
 else 
-    echo "$R you should run this command as a root user $N"
+    echo -e "$R you should run this command as a root user $N"
     exit 1
 fi
