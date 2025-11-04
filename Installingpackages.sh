4003123a35118c0960b5859b59441628a22e224d
mani G <gundlamanikumar@gmail.com>
2025-11-04 09:11:29 -0400

    addding a script

--- Installingpackages.sh
+++ Installingpackages.sh
@@ -13,3 +13,13 @@ else
     echo -e "$R you should run this command as a root user $N"
     exit 1
 fi
+
+yum install mongodb -y
+
+if [ $? -eq 0 ]
+then 
+    echo -e "$G installing mongodb successful $N"
+else 
+    echo -e "$R installing mongodb failed $N"
+    exit 1
+fi
